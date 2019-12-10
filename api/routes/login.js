6be983ee85ef69f96510c37e5d7af32ac72e39cf
Tharit Tiewjarernsopa <tharit.t@ginkgosoft.co.th>
2019-12-10 14:43:38 +0700

    edit regis

--- api/routes/login.js
+++ api/routes/login.js
@@ -3,20 +3,21 @@ const User = require("../../models/user");
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-    User.find({
+    console.log(req.body.userName);
+    console.log(req.body.password);
+    User.findOne({
         'userName': req.body.userName,
         'password': req.body.password
     })
         .exec()
         .then(user => {
+            console.log(user);
             if (!user) {
                 return res.status(404).json({
                     message: "No User."
                 })
             }
-            res.status(200).json({
-                status: "200"
-            })
+            res.status(200).json
         })
 });
 
--- api/routes/user.js
+++ api/routes/user.js
@@ -5,19 +5,31 @@ const router = express.Router();
 
 router.post('/insert', (req, res, next) => {
     console.log(req.body);
-    const user = new User({
-        userName: req.body.userName,
-        password: req.body.password
+    User.findOne({
+        'userName': req.body.userName
     })
-    user
-        .save()
-        .then(result => {
-            console.log(result)
+        .exec()
+        .then(user => {
+            if (user) {
+                return res.status(409).json({
+                    message: "Username was duplicated. Please change your username."
+                })
+            } else {
+                const user = new User({
+                    userName: req.body.userName,
+                    password: req.body.password
+                })
+                user
+                    .save()
+                    .then(result => {
+                        console.log(result)
+                    })
+                    .catch(err => console.log(err));
+                res.status(200).json({
+                    createdUser: user
+                })
+            }
         })
-        .catch(err => console.log(err));
-    res.status(200).json({
-        createdUser: user
-    })
 });
 
 module.exports = router;
